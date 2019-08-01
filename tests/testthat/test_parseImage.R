97b291c08d5be6bd73f48f8635cb06b82b767ae5
nrq2 <rachel.queen@ncl.ac.uk>
2019-08-01 11:07:21 +0100

    Add test for parse image function

--- tests/testthat/test_parseImage.R
+++ tests/testthat/test_parseImage.R
@@ -2,14 +2,14 @@
 # ------------------------------------------------------------------------------
 
 context("Testing parseImage")
-# These tests were created to ensure that the markClusterCol functions works
-# correctly and marks the correct column with Cluster_ prefix
+# These tests were created to ensure that the parseImage functions works
+# correctly and creates a grob
 
 # Test parseImage 
 # ------------------------------------------------------------------------------
 
 # load image
-imgPath <-  "../inst/HE_Rep1_resized.jpg"
+imgPath <-  "../../inst/HE_Rep1_resized.jpg"
 test.grob <- parseImage(imgPath)
 
 test_that("parseImage loads an image and creates a grob", {
