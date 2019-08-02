df4b354ff888f7ff5a8196ed392be45f29729b1c
nrq2 <rachel.queen@ncl.ac.uk>
2019-08-02 12:27:50 +0100

    modified spatialPlot to make it easier to edit

--- R/spatialPlot.R
+++ R/spatialPlot.R
@@ -1,8 +1,7 @@
 # Main Spaniel Plot Function
 # ------------------------------------------------------------------------------
-
-
-ST_plot <- function (Object,  Grob,
+ST_plot <- function (Object,  
+                     Grob,
                      PlotType = c("NoGenes", 
                                   "CountsPerSpot", 
                                   "Cluster", 
@@ -17,91 +16,43 @@ ST_plot <- function (Object,  Grob,
                      pt.size.max = 5)
 
 {
-    ### Validate Object
-    testObject()
-    
-    ### Get Coodinates
-    MetaData = getMetadata(Object)
-    Coordinates = MetaData[, c("x", "y")]
-    Coordinates$spot = rownames(MetaData)
+    ### Validate Object is either a Seurat or SCE object
+    testObject(Object)
     
     ### set title, colour column, and size column according to plotType
-    ungroupVars(plotTitle,cl,sz,shp) %=% 
-        setVars(PlotType, pt.size, Gene)
+    ungroupVars(plotTitle,cl,sz,shp, show_size_legend, colPlot) %=% 
+        setVars(Object, PlotType, pt.size, Gene)
     
-    ## convert shp to NULL
-    if(shp == "NULL"){shp = NULL}
+            # convert shp NULL
+            if(shp == "NULL"){shp = NULL}
+            # convert size legend to logical
+            show_size_legend = ifelse(show_size_legend == "TRUE", T, F)
+            # convert sz to numeric
+            if (suppressWarnings(!is.na(as.numeric(sz)))){
+                sz = as.numeric(sz)
+            }
     
-    ## convert size legend to logical
-    show_size_legend = ifelse(show_size_legend == "TRUE", T, F)
-    
-    ## convert sz to numeric
-    if (suppressWarnings(!is.na(as.numeric(sz)))){
-        sz = as.numeric(sz)
-    }
+    ### create data.frame for ggplot
+    tmp <- make_ggdf(Object, PlotType, colPlot, cl)
     
     
-    ## create data.frame
-    
-    # 1 get column to plot
-    colPlot <- getColPlot(Object, PlotType, ClusterRes)
-    
-    # 2 create tmp
-    
-    ## 3 add QC filter
+    ### Update tmp optional arguments if supplied
     if (!is.null(ShowFilter)){
         tmp$Filter = ShowFilter
         cl = "Filter"
     }
     
-    
-    
-    
-    
-    
-    ## add QC filter
-    
-    
-    
-    
-    if (PlotType == "CountsPerSpot") {
-        
-        
-## add QC filter
-if (!is.null(ShowFilter)){
-    tmp$Filter = ShowFilter
-    cl = "Filter"
-}
-    }
-    
-    
-    if (PlotType == "Cluster") {
-        
-    }
-    
-    if (PlotType == "Gene") {
-        Exprs = getExprs(Object)
-        if(Gene %in% rownames(Exprs) == FALSE){
-            print(paste(Gene, "not found in data. Check rownames"))
-            
-        }
-        tmp = data.frame(Exp = Exprs[Gene, ])
-        tmp$spot = rownames(tmp)
-       
-    }
-    
-    tmp = tmp %>% as.tbl %>% inner_join(Coordinates)
-    tmp$y = 36 - tmp$y
     if (!is.null(CustomTitle)) {
         plotTitle = CustomTitle
     }
-    print(show_size_legend)
-    #tmp$y = tmp$y * -1
+    
+    ### Create plot
     p = plotImage(Grob, tmp, 
                   Colour = cl, 
                   Size = sz, 
                   PlotTitle = plotTitle, 
                   ShowSizeLegend = show_size_legend)
+    
     
     ### FOR QC PLOTS
     if (!is.null(ShowFilter)){
@@ -127,24 +78,7 @@ if (!is.null(ShowFilter)){
                                           pt.size))
     }
     
-    # if (PlotType != "Cluster" & is.null(ShowFilter)){
-    #     p = p +
-    #         scale_colour_gradient(low="#ff3300", high="#ffff00")+
-    #         #scale_size_continuous(range=c(pt.size.min,
-    #         #                   pt.size.max)) +
-    #         guides(color=guide_legend(), size = guide_legend())
-    # }
-    # if (PlotType == "Cluster"){
-    #     p = p + ggplot2::guides(size=FALSE)
-    # } 
-    
-    
-    
-    
-    p
-    
-    
-    
+    return(p)
 }
 
 
