097c5d85b7f9ed5042021a2a14407dc433b82a07
Adrienne <nau13@newcastle.ac.uk>
2022-09-26 15:18:15 +0100

    remove seurat

--- R/createObjects.R
+++ R/createObjects.R
@@ -1,64 +1,7 @@
-#'@rawNamespace import(Seurat, except = "Assays")
 #'@import SingleCellExperiment
 #'@import SummarizedExperiment
 
 NULL
-
-#' Create a Seurat Object From Spatial Transcriptomics Data
-#'
-#' This function converts a count matrix into a Seurat object. The barcodes for
-#' each spot are added to the metadata of the Seurat object and are used in
-#' plotting the data.
-#'
-#' @param counts Raw count matrix or data frame where each row represents a
-#'               gene and each column represents barcoded location on a spatial
-#'               transcriptomics slide. The columns should be named using the
-#'               spot barcode (eg "GTCCGATATGATTGCCGC")
-#' @param barcodeFile a tab seperated barcode file supplied by Spatial
-#'               Trancscriptomics. The file should contains three column:
-#'               The first column contains the Spatial Transcriptomics barcode,
-#'               the second and third column equate to the x and y location
-#' @param projectName The name of the project which is stored in the Seurat
-#'                    Object.
-#' @param sectionNumber The location of the sample on the slide
-#' @return A Seurat Object
-#' 
-#' @usage  createSeurat(counts, barcodeFile, projectName = projectName,
-#'                         sectionNumber = sectionNo)
-#'  
-#' 
-#' @export
-#' @examples
-#' ## Data is taken from DOI: 10.1126/science.aaf2403
-#' examplecounts <- readRDS(file.path(system.file(package = "Spaniel"),
-#'                             "extdata/counts.rds"))
-#' exampleBarcodes <- file.path(system.file(package = "Spaniel"),
-#'                             "1000L2_barcodes.txt")
-#' SeuratObj <- createSeurat(examplecounts,
-#'                         exampleBarcodes,
-#'                         projectName = "TestProj",
-#'                         sectionNumber = 1
-#'                         )
-
-
-createSeurat <- function(counts,
-                        barcodeFile,
-                        projectName = "projectName",
-                        sectionNumber = "sectionNo") {
-    barcodes <- utils::read.csv(barcodeFile, sep = "\t", header = FALSE)
-    rownames(barcodes) <- barcodes$V1
-    barcodes <- barcodes[colnames(counts),]
-    colnames(barcodes) <- c("spot", "x", "y")
-    seuratObj <- Seurat::CreateSeuratObject(counts = counts,
-                                            project = paste0(projectName,
-                                                                sectionNumber,
-                                                                sep = "_"))
-    seuratObj@meta.data[, c("spot",
-                            "x", "y")] <- barcodes[, c("spot",
-                                                        "x", "y")]
-    return(seuratObj)
-    
-}
 
 
 #' Create a SingleCellExperiment Object From Spatial Transcriptomics Data
